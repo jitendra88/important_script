b49b54ee9cc54881ca3478576a6bc2a07c26521d
jitendra88 <jd.aligarh@gmail.com>
2017-06-09 03:45:04 +0530

    base version

--- chat_data_migration_v2_to_v3.py
+++ chat_data_migration_v2_to_v3.py
@@ -168,6 +168,7 @@ def get_chat_data_from_v2(page):
     #print "============================= duplicate message count is :" + str(len(duplicate_msg_id_list))
     print "============================= script completed ==================================================="
     wb.save(filename=dest_filename)
+    pool.close()
     print "============================= please check error report file ==========================================="
     exit()
 
