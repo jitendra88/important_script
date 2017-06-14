bef46530b26a290c5159ced332ca59130ca917b6
jitendra88 <jd.aligarh@gmail.com>
2017-06-14 18:26:05 +0530

    change

--- chat_migration_start.py
+++ chat_migration_start.py
@@ -4,4 +4,4 @@ print "======================== chat migration start============================
 
 for i in (1,2):
     commands.getoutput("screen -dmS chat_message_part_"+str(i)+" bash")
-    commands.getoutput("screen -S chat_message_part_"+str(i)+" -p 0 -X exec python chat_data_migration_v2_to_v3.py "+str(i)+"")
+    commands.getoutput("screen -S chat_message_part_"+str(i)+" -p 0 -X exec sudo python chat_data_migration_v2_to_v3.py "+str(i)+"")
