dbb8387461497755eafa24e33aa85c0da2d56e84
jitendra88 <jd.aligarh@gmail.com>
2017-06-15 10:38:57 +0530

    change

--- chat_data_migration_v2_to_v3.py
+++ chat_data_migration_v2_to_v3.py
@@ -39,7 +39,7 @@ CHAT_TYPE_IMAGE_V2 = 'vImage'
 CHAT_TYPE_TEXT_V2 = 'vText'
 CHAT_TYPE_IMAGE_V3 = 'chat_image'
 CHAT_TYPE_TEXT_V3 = 'chat_text'
-PAGINATION_LIMIT = 100000
+PAGINATION_LIMIT = 20000
 TOTAL_NO_PROCESS = 1
 page = None
 if sys.argv[1:]:
@@ -110,7 +110,7 @@ def get_chat_data_from_v2(page):
     print "================Pagination Limit  :===" + str(PAGINATION_LIMIT)
 
     cur2.execute(
-        "SELECT * FROM ofMessageArchive WHERE  messageID NOT IN  (SELECT message_id from deleted_messages) ORDER BY messageID DESC limit " + str(
+        "SELECT fromJID,toJID,sentDate,body FROM ofMessageArchive WHERE  messageID NOT IN  (SELECT message_id from deleted_messages) ORDER BY messageID DESC limit " + str(
             start) + " ," + str(PAGINATION_LIMIT) + "")
     print "================Total Message count In ofMessageArchive Table :===" + str(cur2.rowcount)
 
