fdb9bdc51f567e2389e17ee2c15b55429cc1140d
jitendra88 <jd.aligarh@gmail.com>
2017-06-20 11:58:42 +0530

    chnage in chat migration

--- chat_migration_start.py
+++ chat_migration_start.py
@@ -6,34 +6,36 @@ from openpyxl.workbook import Workbook
 user_obj = {}
 delete_message_obj = {}
 
-print "======================== chat migration start==================================================\N"
 
-# ---------------------------------- V3 ---------------------------------------------------
-con_v3 = mdb.connect('beta-v3-1.ctjt9fapuyu7.eu-west-1.rds.amazonaws.com', 'root', 'myuroot123', 'myuv3');
-cur_3 = con_v3.cursor(mdb.cursors.DictCursor)
-cur_3.execute('SELECT id,idV2 FROM users WHERE  idV2 is NOT  NULL ')
+def default_start():
+    print "======================== chat migration start==================================================\N"
 
-print "===================use object creation started ................."
-for row in cur_3.fetchall():
-    idV2 = str(row["idV2"])
-    idV3 = str(row["id"])
-    user_obj[idV2] = idV3
-con_v3.close()
-print "=======================end======================"
+    # ---------------------------------- V3 ---------------------------------------------------
+    con_v3 = mdb.connect('beta-v3-1.ctjt9fapuyu7.eu-west-1.rds.amazonaws.com', 'root', 'myuroot123', 'myuv3');
+    cur_3 = con_v3.cursor(mdb.cursors.DictCursor)
+    cur_3.execute('SELECT id,idV2 FROM users WHERE  idV2 is NOT  NULL ')
+
+    print "===================use object creation started ................."
+    for row in cur_3.fetchall():
+        idV2 = str(row["idV2"])
+        idV3 = str(row["id"])
+        user_obj[idV2] = idV3
+    con_v3.close()
+    print "=======================end======================"
 
 
-# ---------------------------------- V2 ---------------------------------------------------
-con_v2 = mdb.connect('beta-php.c03pbdmxnxpo.eu-west-1.rds.amazonaws.com', 'root', 'admin123*', 'myu');
-cur_2 = con_v2.cursor(mdb.cursors.DictCursor)
-cur_2.execute('SELECT message_id,user_id FROM deleted_messages')
+    # ---------------------------------- V2 ---------------------------------------------------
+    con_v2 = mdb.connect('beta-php.c03pbdmxnxpo.eu-west-1.rds.amazonaws.com', 'root', 'admin123*', 'myu');
+    cur_2 = con_v2.cursor(mdb.cursors.DictCursor)
+    cur_2.execute('SELECT message_id,user_id FROM deleted_messages')
 
-print "===================delete message object creation started ................."
-for row in cur_2.fetchall():
-    message_id = str(row["message_id"])
-    user_id = str(row["user_id"])
-    delete_message_obj[message_id] = user_id
-con_v2.close()
-print "=======================end======================"
+    print "===================delete message object creation started ................."
+    for row in cur_2.fetchall():
+        message_id = str(row["message_id"])
+        user_id = str(row["user_id"])
+        delete_message_obj[message_id] = user_id
+    con_v2.close()
+    print "=======================end======================"
 
 
 
@@ -58,4 +60,13 @@ def create_screen(start, end):
             i) + " -p 0 -X exec sudo python chat_data_migration_v2_to_v3.py " + str(i) + "")
 
 
-create_screen(1,10)
+import sys
+page = None
+if sys.argv[1:]:
+    page = sys.argv[1:][0]
+else:
+    print "Please put cmd line argument..............."
+    exit()
+if page:
+    default_start()
+    create_screen(1,10)
