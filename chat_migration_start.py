d813e01e670a37dddc28b33465d52b72fa7e65c6
jitendra88 <jd.aligarh@gmail.com>
2017-06-20 11:49:14 +0530

    chnage in chat migration

--- chat_migration_start.py
+++ chat_migration_start.py
@@ -56,3 +56,6 @@ def create_screen(start, end):
         commands.getoutput("screen -dmS chat_message_part_" + str(i) + " bash")
         commands.getoutput("screen -S chat_message_part_" + str(
             i) + " -p 0 -X exec sudo python chat_data_migration_v2_to_v3.py " + str(i) + "")
+
+
+create_screen(1,10)
