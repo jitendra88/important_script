25234412393dd6b10d72454c4833289d3b135dcf
jitendra88 <jd.aligarh@gmail.com>
2017-06-20 13:11:08 +0530

    change in notification

--- chat_migration_start.py
+++ chat_migration_start.py
@@ -51,11 +51,6 @@ ws1.append(header)
 
 
 # ================================ end ============================================================================#
-for i in range(1, 10):
-    print " screen -dmS chat_message_part_" + str(i) + ""
-    commands.getoutput("screen -dmS chat_message_part_" + str(i) + " bash")
-    commands.getoutput("screen -S chat_message_part_" + str(
-        i) + " -p 0 -X exec sudo python chat_data_migration_v2_to_v3.py " + str(i) + "")
 
 
 
---
+++ chat_screen_creation.py
@@ -0,0 +1,7 @@
+import commands
+
+for i in range(1, 10):
+    print " screen -dmS chat_message_part_" + str(i) + ""
+    commands.getoutput("screen -dmS chat_message_part_" + str(i) + " bash")
+    commands.getoutput("screen -S chat_message_part_" + str(
+        i) + " -p 0 -X exec sudo python chat_data_migration_v2_to_v3.py " + str(i) + "")
